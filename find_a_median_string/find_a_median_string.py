7acf5cf8ebf46dd254b5ea55d7cee53be35abc95
fatma.kahveci <fatmaba@gmail.com>
2017-10-02 00:21:40 +0300

    find a median string is completed

--- find_a_median_string/find_a_median_string.py
+++ find_a_median_string/find_a_median_string.py
@@ -4,28 +4,34 @@ import itertools
 
 file_name='find_a_median_string.txt'
 
-def hammming_distance(pattern1, pattern2):
+def hamming_distance(pattern1, pattern2):
 	d=0
-	for i in xrange(len(pattern1)):
+	for i in range(len(pattern1)):
 		if pattern1[i]==pattern2[i]:
 			d+=1
 	return d
 
+def find_all_possible_kmers(sequences,k):
+	nucleotides = ['A', 'C', 'G', 'T']
+	nucleotide_combinations = list(itertools.product(nucleotides, repeat=k))
+	for i in range(len(nucleotide_combinations)):
+		nucleotide_combinations[i] = ''.join(nucleotide_combinations[i])
+	return nucleotide_combinations
+
 def median_string(sequences,k):
-    print list(itertools.permutations([A C G T],4))
-    # distance = float('inf')
-    # median = ""
-    # for kmer in :
-        # d = hammming_distance(pattern,)
-        # if d <= distance:
-            # distance = d
-            # median = pattern
-	# return median
+	all_kmers=find_all_possible_kmers(sequences,k)
+	count=[0]*len(all_kmers)
+	for i in range(len(sequences)):
+		sequence=sequences[i]
+		for j in range(len(sequence)-k):
+			seq=sequence[j:j+k]
+			count[all_kmers.index(seq)]+=1
+	print(all_kmers[count.index(max(count))])
 
 def main():
 	file_content=list(open(file_name,'r').read().split('\n'))
 	k=int(file_content[0])
-	sequences=map(str,file_content[1:])
+	sequences=file_content[1:]
 	median_string(sequences,k)
 
 if __name__ == '__main__':
