d659d40c2a316f0fc099b0e92e1bb31f147705ba
fatma.kahveci <fatmaba@gmail.com>
2017-10-02 16:45:08 +0300

    correction

--- find_a_median_string/find_a_median_string.py
+++ find_a_median_string/find_a_median_string.py
@@ -1,19 +1,12 @@
 #!/bin/env/python
 
 import itertools
+import sys
 
 file_name = 'find_a_median_string.txt'
 
 
-def hamming_distance(pattern1, pattern2):
-    d = 0
-    for i in range(len(pattern1)):
-        if pattern1[i] == pattern2[i]:
-            d += 1
-    return d
-
-
-def find_all_possible_kmers(sequences, k):
+def find_all_possible_kmers(k):
     nucleotides = ['A', 'C', 'G', 'T']
     nucleotide_combinations = list(itertools.product(nucleotides, repeat=k))
     for i in range(len(nucleotide_combinations)):
@@ -21,23 +14,39 @@ def find_all_possible_kmers(sequences, k):
     return nucleotide_combinations
 
 
+def hamming_distance(pattern1, pattern2):
+    d = 0
+    for i in range(len(pattern1)):
+        if pattern1[i] != pattern2[i]:
+            d += 1
+    return d
+
+
+def total_distance(pattern, sequences,k):
+    distance = 0
+    for sequence in sequences:
+        min_hamming = sys.maxsize
+        for i in range(len(sequence) - k + 1):
+            min_hamming = min(min_hamming, hamming_distance(pattern, sequence[i:i + k]))
+        distance += min_hamming
+    return distance
+
+
 def median_string(sequences, k):
-    all_kmers = find_all_possible_kmers(sequences, k)
-    count = [0] * len(all_kmers)
-    for i in range(len(sequences)):
-        sequence = sequences[i]
-        for j in range(len(sequence) - k):
-            seq = sequence[j:j + k]
-            count[all_kmers.index(seq)] += 1
-
-    print(all_kmers[count.index(max(count))])
-
+    best_pattern = sequences[0][:k]
+    best_distance = k
+    for kmer in find_all_possible_kmers(k):
+        d = total_distance(kmer, sequences,k)
+        if d <= best_distance:
+            best_distance = d
+            best_pattern = kmer
+    return best_pattern
 
 def main():
     file_content = list(open(file_name, 'r').read().split('\n'))
     k = int(file_content[0])
     sequences = file_content[1:]
-    median_string(sequences, k)
+    print(median_string(sequences, k))
 
 
 if __name__ == '__main__':
