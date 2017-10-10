7b87bd38b01da5e7f6bd5a061834a97845465c75
fatma.kahveci <fatmaba@gmail.com>
2017-10-10 15:06:38 +0300

    construct_a_trie_from_a_collection_of_patterns is added

---
+++ construct_a_trie_from_a_collection_of_patterns/construct_a_trie_from_a_collection_of_patterns.py
@@ -0,0 +1,10 @@
+#!/bin/env/python
+
+file_name='construct_a_trie_from_a_collection_of_patterns.txt'
+
+def main():
+    patterns=open(file_name,'r').read().split('\n')
+
+
+if __name__=="__main__":
+    main()
