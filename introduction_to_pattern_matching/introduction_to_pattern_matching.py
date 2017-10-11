eb0e36431b75ffc703b84ece9f84867a3f440eb9
fatma.kahveci <fatmaba@gmail.com>
2017-10-12 02:17:15 +0300

    introduction_to_pattern_matching is added

--- introduction_to_pattern_matching/introduction_to_pattern_matching.py
+++ introduction_to_pattern_matching/introduction_to_pattern_matching.py
@@ -1,26 +1,38 @@
 #!/bin/env/python
 
-from collections import defaultdict
+from itertools import count
 
-file_name = "introduction_to_pattern_matching.txt"
+file_name = 'introduction_to_pattern_matching.txt'
+
+
+class Trie(object):
+    def __init__(self):
+        self.counter = count(start=1)
+        self.root = [next(self.counter), {}]
+
+
+def write_result(parent):
+    parents_child = parent[1]
+    parents_parent = parent[0]
+    for character, child in parents_child.items():
+        childs_parent = child[0]
+        print(parents_parent, childs_parent, character)
+        write_result(child)
 
 
 def main():
-    strings = list(open(file_name, 'r').read().split('\n'))
-    trie=defaultdict(list)
-    char_index=2
-
+    strings = open(file_name, 'r').read().split('\n')
+    trie = Trie()
     for string in strings:
-        start_index=1
-        for char in string:
-            if char not in trie[start_index]:
-                trie[start_index].append(char)
-                if char not in trie[start_index-1]:
-                    print(str(start_index)+' ' + str(char_index) + ' ' + char)
-                else:
-                    print(str(char_index-1) + ' ' + str(char_index) + ' ' + char)
-                char_index += 1
-            start_index+=1
+        node = trie.root
+        for character in string:
+            nodes_child = node[1]
+            if character not in nodes_child:
+                nodes_child[character] = [next(trie.counter), {}]
+            node = nodes_child[character]
 
-if __name__ == "__main__":
+    write_result(trie.root)
+
+
+if __name__ == '__main__':
     main()
