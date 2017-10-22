274b0d4ffb55a5e57f4f75683d10866a8fa656ba
fatma.kahveci <fatmaba@gmail.com>
2017-10-22 12:47:16 +0300

    global_alignment_with_scoring_matrix is added

---
+++ global_alignment_with_scoring_matrix/global_alignment_with_scoring_matrix.py
@@ -0,0 +1,48 @@
+#!/bin/env/python
+
+from Bio import SeqIO
+from Bio.SubsMat.MatrixInfo import blosum62
+
+file_name = 'global_alignment_with_scoring_matrix.txt'
+
+
+def get_max_alignment(seq1, seq2, gap_penalty):
+    m = len(seq1)
+    n = len(seq2)
+
+    alignment_matrix = {(0, 0): (0, None)}
+    alignment_matrix.update({((i, 0), (i * - gap_penalty, (i - 1, 0))) for i in range(1, m + 1)})
+    alignment_matrix.update({((0, i), (i * - gap_penalty, (0, i - 1))) for i in range(1, n + 1)})
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            cost = blosum62.get((seq1[i - 1], seq2[j - 1]))
+
+            if cost == None:
+                cost = blosum62.get((seq2[j - 1], seq1[i - 1]))
+
+            max_value = max(alignment_matrix[(i - 1, j - 1)][0] + cost, alignment_matrix[(i - 1, j)][0] - gap_penalty, alignment_matrix[(i, j - 1)][0] - gap_penalty)
+
+            if alignment_matrix[(i - 1, j - 1)][0] + cost == max_value:
+                alignment_matrix[(i, j)] = (max_value, (i - 1, j - 1))
+            elif alignment_matrix[(i - 1, j)][0] - gap_penalty == max_value:
+                alignment_matrix[(i, j)] = (max_value, (i - 1, j))
+            elif alignment_matrix[(i, j - 1)][0] - gap_penalty == max_value:
+                alignment_matrix[(i, j)] = (max_value, (i, j - 1))
+
+    return alignment_matrix[(i, j)][0]
+
+
+def main():
+    sequences = list(SeqIO.parse(file_name, 'fasta'))
+
+    seq1 = sequences[0]
+    seq2 = sequences[1]
+
+    gap_penalty = 5
+
+    print(get_max_alignment(seq1, seq2, gap_penalty))
+
+
+if __name__ == '__main__':
+    main()
