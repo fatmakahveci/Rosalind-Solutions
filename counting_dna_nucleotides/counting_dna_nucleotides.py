b8c334170d6741aa5160f671b4f8cec1ccf9e151
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-23 23:18:27 +0300

    Update counting_dna_nucleotides.py

--- counting_dna_nucleotides/counting_dna_nucleotides.py
+++ counting_dna_nucleotides/counting_dna_nucleotides.py
@@ -1,27 +1,8 @@
 #!/bin/env/python
 
-file_name = 'counting_dna_nucleotides.txt'
-
-
-def main():
-    num_a = 0
-    num_c = 0
-    num_g = 0
-    num_t = 0
-
-    file_content = open(file_name, 'r').read()
-
-    for i in range(len(file_content)):
-        if file_content[i] == 'A':
-            num_a += 1
-        elif file_content[i] == 'C':
-            num_c += 1
-        elif file_content[i] == 'G':
-            num_g += 1
-        elif file_content[i] == 'T':
-            num_t += 1
-    print(str(num_a) + ' ' + str(num_c) + ' ' + str(num_g) + ' ' + str(num_t))
-
-
 if __name__ == "__main__":
-    main()
+    file = open("rosalind_dna.txt","r")
+    dna_string = str(file.readlines()).strip()
+    base_count_list = map(str, [dna_string.count('A'),dna_string.count('C'),dna_string.count('G'),dna_string.count('T')])
+    print(' '.join(base_count_list))
+    file.close()
