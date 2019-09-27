c5460dc3c22f835ff7c5c7e0a8ba5d2f457fffaf
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-28 00:46:03 +0300

    Update working_with_files.py

--- working_with_files/working_with_files.py
+++ working_with_files/working_with_files.py
@@ -1,16 +1,7 @@
 #!/bin/env/python
 
-file_name = 'working_with_files.txt'
-
-
-def main():
-    with open(file_name, 'r') as file:
-        num = 0
-        for line in file:
-            num += 1
-            if num % 2 == 0:
-                print(line.strip())
-
-
 if __name__ == "__main__":
-    main()
+    with open("rosalind.txt","r") as file:
+    	for idx, line in enumerate(file, start=1):
+        	if idx % 2 == 0:
+        		print(line.strip())
