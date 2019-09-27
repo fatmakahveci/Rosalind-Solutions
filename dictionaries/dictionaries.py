ba628af5887146106e735a193de7b94e72426123
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-28 00:51:16 +0300

    Update dictionaries.py

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -1,17 +1,9 @@
 #!/bin/env/python
 
-file_name = 'dictionaries.txt'
+if __name__ == "__main__":
 
-
-def main():
-    file_content = open(file_name, 'r').read().split(' ')
-    a = set(file_content)
+    file_content = open("rosalind.txt", 'r').read().split(' ')
+    a = list(set(file_content))
     b = [file_content.count(e) for e in a]
-    a = list(a)
-    b = list(b)
     for i in range(len(a)):
         print(a[i] + ' ' + str(b[i]))
-
-
-if __name__ == "__main__":
-    main()
