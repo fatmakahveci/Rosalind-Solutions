14a186d36641e13246d38a6af8068df09fdbf04b
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-28 00:37:09 +0300

    Update conditions_and_loops.py

--- conditions_and_loops/conditions_and_loops.py
+++ conditions_and_loops/conditions_and_loops.py
@@ -1,19 +1,5 @@
 #!/bin/env/python
 
-file_name = 'conditions_and_loops.txt'
-
-
-def main():
-    variables = open(file_name, 'r').read().split(' ')
-    a = int(variables[0])
-    b = int(variables[1])
-
-    ans = 0
-    for num in range(a, b):
-        if num % 2 == 1:
-            ans += num
-    print(ans)
-
-
 if __name__ == "__main__":
-    main()
+    a,b = map(int, open('rosalind.txt', 'r').readline().split(' '))
+    print(sum(list(filter(lambda x: x % 2 == 1, range(a, b)))))
