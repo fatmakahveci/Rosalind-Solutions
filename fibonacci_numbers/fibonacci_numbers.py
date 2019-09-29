3d039c8f7cd8560bc081af9be5eea0abdb293eca
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-29 20:16:20 +0300

    Update fibonacci_numbers.py

--- fibonacci_numbers/fibonacci_numbers.py
+++ fibonacci_numbers/fibonacci_numbers.py
@@ -1,20 +1,15 @@
 #!/bin/env/python
 
-file_name = 'fibonacci_numbers.txt'
-
-
 def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fibonacci(n-1) + fibonacci(n-2)
 
+if __name__ == "__main__":
 
-def main():
-    n = int(open(file_name, 'r').read().strip())
-    print(fibonacci(n))
-
-
-if __name__ == '__main__':
-    main()
+	with open("rosalind.txt", 'r') as file:
+		n = int(str(file.read()).strip())
+		print(fibonacci(n))
+		file.close()
