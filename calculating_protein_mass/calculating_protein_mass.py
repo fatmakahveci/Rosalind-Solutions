f27c92e99c810da7f567e2a707a79601dc4cf69c
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-30 12:38:38 +0300

    Update calculating_protein_mass.py

--- calculating_protein_mass/calculating_protein_mass.py
+++ calculating_protein_mass/calculating_protein_mass.py
@@ -1,27 +1,17 @@
-#!/bin/env/python
-
-file_name = 'calculating_protein_mass.txt'
+#!/bin/env/python3.7
 
 
-def read_table(file_name):
+if __name__ == '__main__':
+    
     amino_acid = []
     amino_acid_mass = []
 
-    for line in open(file_name, 'r').read().split('\n'):
+    for line in open('monoisotopic_mass_table.txt', 'r').read().split('\n'):
         line = line.replace(' ', '')
         amino_acid.append(line[0])
         amino_acid_mass.append(float(line[1:]))
-    return amino_acid, amino_acid_mass
 
-
-def read_protein(file_name):
-    protein = open(file_name, 'r').read()
-    return protein
-
-
-def main():
-    amino_acid, amino_acid_mass = read_table('../tables/monoisotopic_mass_table.txt')
-    protein = read_protein(file_name)
+    protein = open("rosalind.txt","r").read()
 
     sum = 0
 
@@ -29,7 +19,3 @@ def main():
         sum += amino_acid_mass[amino_acid.index(protein[i])]
 
     print(round(sum, 3))
-
-
-if __name__ == '__main__':
-    main()
