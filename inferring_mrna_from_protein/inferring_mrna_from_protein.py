186459ecd347982eddd3adb7922f6ca6a2540f1e
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-30 12:27:33 +0300

    Update inferring_mrna_from_protein.py

--- inferring_mrna_from_protein/inferring_mrna_from_protein.py
+++ inferring_mrna_from_protein/inferring_mrna_from_protein.py
@@ -1,41 +1,30 @@
-#!/bin/env/python
-
-file_name = 'inferring_mrna_from_protein.txt'
-
-
-def codon_alphabet():
-    alphabet_file_content = open('../tables/rna_codon_table.txt', 'r').read().replace('\n', '').replace('\t','').replace(' ','')
-    dict_codon = {}
-    i = 0
-    while i <= len(alphabet_file_content) - 4:
-        s = alphabet_file_content[i:i + 3]
-        r = alphabet_file_content[i + 3]
-        if r == 'X':
-            dict_codon[s] = 'Stop'
-        else:
-            dict_codon[s] = r
-        i += 4
-    return dict_codon
-
-
-def main():
-    protein = open(file_name, 'r').read()
-    codon_value_set = list(set(codon_alphabet().values()))
-    count = [0] * len(codon_value_set)
-    i = 0
-    for value in codon_value_set:
-        for element in codon_alphabet().values():
-            if element == value:
-                count[i] += 1
-        i += 1
-    
-    stop_codon = count[codon_value_set.index('Stop')]
-    result = stop_codon
-    for p in protein:
-        result *= count[codon_value_set.index(p)]
-    print(result % 1000000)
+#!/bin/env/python3.7
 
 
 if __name__ == '__main__':
-    main()
 
+	with open("rna_codon_table.txt", 'r') as file:
+		item_list = file.read().split()
+		file.close()
+	
+	rna_codon_dict = {}
+	i = 0
+	while i < len(item_list) - 1:
+		rna_codon_dict[item_list[i]] = item_list[i+1]
+		i += 1
+
+	protein = open("rosalind.txt", 'r').read()
+	codon_value_set = list(set(rna_codon_dict.values()))
+	count = [0] * len(codon_value_set)
+	i = 0
+	for value in codon_value_set:
+	    for element in rna_codon_dict.values():
+	        if element == value:
+	            count[i] += 1
+	    i += 1
+    
+	stop_codon = count[codon_value_set.index('Stop')]
+	result = stop_codon
+	for p in protein:
+	    result *= count[codon_value_set.index(p)]
+	print(result % 1000000)
