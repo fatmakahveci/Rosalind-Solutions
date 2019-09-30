47846474fe26d2f5eee447c30c51d77c6498d8ca
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-30 10:39:21 +0300

    Update mendels_first_law.py

--- mendels_first_law/mendels_first_law.py
+++ mendels_first_law/mendels_first_law.py
@@ -1,61 +1,12 @@
-#!/bin/env/python
+#!/bin/env/python3.7
 
-file_name = 'mendels_first_law.txt'
+def dominant_probability(dom, het, rec):
+	total = dom + het + rec
 
-variable_list = []
-
-
-def readfile():
-    return open(file_name, 'r').read()
-
-
-def get_variables():
-    for i in range(len(readfile().split(' '))):
-        variable_list.append(float(readfile().split(' ')[i]))
-    return variable_list
-
-
-def main():
-    dom = get_variables()[0]
-    het = get_variables()[1]
-    rec = get_variables()[2]
-    total = dom + het + rec
-
-    result = 1 - ((2 * 0.5 * (het * rec) / (total * (total - 1))) + (rec * (rec - 1)) / (total * (total - 1)) + (het * (het - 1)) / (total * (total - 1)) * 0.25)
-
-    print("%.5f" % result)
-
+	return 1 - ((2 * 0.5 * (het * rec) / (total * (total - 1))) + (rec * (rec - 1)) / (total * (total - 1)) + (het * (het - 1)) / (total * (total - 1)) * 0.25)
 
 if __name__ == '__main__':
-    main()
-#!/bin/env/python
-
-file_name = 'mendels_first_law.txt'
-
-variable_list = []
-
-
-def readfile():
-    return open(file_name, 'r').read()
-
-
-def get_variables():
-    for i in xrange(len(readfile().split(' '))):
-        variable_list.append(float(readfile().split(' ')[i]))
-    return variable_list
-
-
-def main():
-    dom = get_variables()[0]
-    het = get_variables()[1]
-    rec = get_variables()[2]
-    total = dom + het + rec
-    
-    result = 1 - ((2 * 0.5 * (het * rec) / (total * (total - 1))) + (rec * (rec - 1)) / (total * (total - 1)) + (het * (het - 1)) / (
-                                                                           total * (total - 1)) * 0.25)
-                                                                                                                                     
-    print("%.5f" % result)
-
-
-if __name__ == '__main__':
-    main()
+	with open("rosalind.txt", 'r') as file:
+		dom, het, rec = map(float, file.readline().split(' '))
+		file.close()
+	print("%.5f" % dominant_probability(dom, het, rec))
