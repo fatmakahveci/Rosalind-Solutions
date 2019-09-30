c9ef1a9a14c5c9a6d79e62c85c6b097454d45baf
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-09-30 12:01:18 +0300

    Update calculating_expected_offspring.py

--- calculating_expected_offspring/calculating_expected_offspring.py
+++ calculating_expected_offspring/calculating_expected_offspring.py
@@ -1,22 +1,6 @@
-#!/bin/env python
-
-file_name = 'calculating_expected_offspring.txt'
-
-
-def get_sequences(file_name):
-    return open(file_name, 'r').read().split(' ')
-
-
-def calculating_expected_offspring(file_name):
-    genotype_list = map(float, get_sequences(file_name))
-
-    return 2 * (
-    genotype_list[0] + genotype_list[1] + genotype_list[2] + genotype_list[3] * 3 / 4 + genotype_list[4] / 2)
-
-
-def main():
-    print(str(calculating_expected_offspring(file_name)))
-
+#!/bin/env python3.7
 
 if __name__ == '__main__':
-    main()
+	genotype_list = map(float, open("rosalind.txt", 'r').read().split(' '))
+
+	print(str(2 * (genotype_list[0] + genotype_list[1] + genotype_list[2] + genotype_list[3] * 3 / 4 + genotype_list[4] / 2)))
