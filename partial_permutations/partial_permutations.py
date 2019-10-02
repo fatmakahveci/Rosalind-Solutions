383cbd0e0c520621d58641ec8676a0b1e6bef5db
Fatma Kahveci <fatma.balci@bilkent.edu.tr>
2019-10-03 00:03:41 +0300

    Update partial_permutations.py

--- partial_permutations/partial_permutations.py
+++ partial_permutations/partial_permutations.py
@@ -1,18 +1,12 @@
-#!/bin/env/python
+#!/usr/bin/env python3.7
 
 
-file_name = 'partial_permutations.txt'
-
-
-def main():
-    n = int(open(file_name, 'r').read().split(' ')[0])
-    k = int(open(file_name, 'r').read().split(' ')[1])
+if __name__ == "__main__":
+    with open("rosalind.txt", 'r') as file:
+        n, k = map(int, file.readline().strip('\n').split(' '))
+        file.close()
 
     permutation = 1
     for i in range(k):
         permutation *= (n - i)
     print(permutation % 1000000)
-
-
-if __name__ == "__main__":
-    main()
